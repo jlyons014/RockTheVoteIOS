09ed4adf098c6f6ad99cee37a7da190e3d2fd3dd
jlyons014 <jlyons014@gmail.com>
2019-12-12 08:10:09 -0500

    Initial Commit

---
+++ RockTheVote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockTheVote
+//
+//  Created by Lyons, Joseph John on 12/12/19.
+//  Copyright © 2019 Lyons, Joseph John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
